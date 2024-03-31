dead09304fe8a38d36a4f3631663f5a1335baf08
Arina8-90mand <mandrykarina@yandex.ru>
2024-03-31 12:28:10 +0300

    app

--- app/app.py
+++ app/app.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 
 @app.route('/index')
-@app.route('/')
 def index():
-    return '<h1>Main Page</h1>'
+    return render_template('index.html')
 
+@app.route('/vvod', methods=['GET', 'POST'])
+def vvod():
+    if request.method == 'POST':
+        data = request.form['data']
+        print(data)
+    return render_template('vvod.html')
 
-@app.route('/about')
-def about():
-    return '<h1>About Page</h1>'
+@app.route('/list')
+def list():
+    return render_template('list.html')
 
 
 if __name__ == '__main__':
