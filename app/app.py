ecd9ffbe92b0146181086593d1b17beb7341c2bc
Arina8-90mand <mandrykarina@yandex.ru>
2024-03-31 10:24:29 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,24 +1,18 @@
-import sys
-import pyqtgraph as pg
+from flask import Flask
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QMessageBox, QVBoxLayout, QTableWidgetItem, QFileDialog
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QTableWidget
-from PyQt5.QtGui import QPixmap, QIcon
-
-import datetime
+app = Flask(__name__)
 
 
-class MyWidget(QMainWindow, QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('design.ui', self)  # Загружаем дизайн
-        self.historybutton.clicked.connect(self.open_second_form)
+@app.route('/index')
+@app.route('/')
+def index():
+    return '<h1>Main Page</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
 
 
 if __name__ == '__main__':
-    # создаем инициализацию приложения
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    app.run(debug=True)
