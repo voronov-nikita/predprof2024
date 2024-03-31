e01697f24929ca4ef315a9f8c6d1da96ecc8acf5
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-03-31 13:45:41 +0300

    add data

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
+import sys
+sys.path.append("../")
+
+from src.database import *
+
 from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+
+createDatabase()
 
 @app.route('/')
 @app.route('/index')
@@ -11,8 +18,20 @@ def index():
 @app.route('/vvod', methods=['GET', 'POST'])
 def vvod():
     if request.method == 'POST':
-        data = request.form['data']
-        print(data)
+        date = request.form['date']
+        number = request.form['number']
+        window = request.form['window']
+        light = request.form['light']
+        
+        print(date, number, window, light)
+        
+        if date and number and window and light:
+            addDataDate(date, "")
+            addDataFloors(number, "")
+            addDataWindows(window, "")
+            addDataRooms(light, "")
+        else:
+            print("Не довведенны необходимые данные")
     return render_template('vvod.html')
 
 @app.route('/list')
